006d7fe07e56a244baa26c03b68df3aca34d4198
zash <danialzash@gmail.com>
2023-08-19 00:26:17 +0330

    Set failed method for Welcome Notifier Listener

--- app/Listener/WelcomeNotifierListener.php
+++ app/Listener/WelcomeNotifierListener.php
@@ -6,6 +6,7 @@ use App\Events\UserRegisteredEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use PHPUnit\Event\Code\Throwable;
 
 class WelcomeNotifierListener implements ShouldQueue
 {
@@ -27,5 +28,17 @@ class WelcomeNotifierListener implements ShouldQueue
 
         // to stop the propagation of an event to other listeners.
         return false;
+    }
+
+    /**
+     * When event listener may fail the listener call this function with corresponding throwable
+     * By now only the throwable message and id of the event user are logged but logics should handle here
+     * @param UserRegisteredEvent $event
+     * @param Throwable $throwable
+     * @return void
+     */
+    public function failed(UserRegisteredEvent $event, Throwable $throwable): void
+    {
+        Log::alert($throwable->message() . ":" . $event->user->id);
     }
 }
